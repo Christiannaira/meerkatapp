cc617eed2538faa6afc2252dc9fce64bbff3d1fa
Christian <Christianmnaira@gmail.com>
2025-05-15 10:14:13 +0800

    feat: initialized form ui and basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,58 @@
+import { useState } from "react"
+
 function App() {
+
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  
+
+
+  const addUser = async () => {
+
+      alert(userEmail + " " + userPassword);
+
+  }
  
 
   return (
     <>
 
-      
+      <div className="container mt-5 p-5 border rounded">
+        <h1 className="text-center text-info">Welcome to Meerkat App</h1>
+
+        <div className="text-center mt-5">
+
+          <h2>Sign Up</h2>
+
+          <form>
+
+            <label htmlFor="email">Email</label> <br />
+            <input 
+            id="email"
+            type="email"  
+            onChange={(event) => {
+              setUserEmail(event.target.value);
+            }}/> <br /> <br />
+
+            <label htmlFor="password">Password</label> <br />
+            
+            <input 
+            id="password"
+            type="password"  
+            onChange={(event) => {
+              setUserPassword(event.target.value);
+            }}/>  <br /> <br />
+            
+            <button type="button" className="btn btn-primary btn-lg" onClick={addUser}>Sign Up</button>
+
+            <p>{userEmail}</p>
+            <p>{userPassword}</p>
+
+          </form>
+
+        </div>
+
+      </div>
 
     </>
   )
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,7 @@
 
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -14,4 +15,7 @@ const firebaseConfig = {
 
 x
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
 const analytics = getAnalytics(app);
